ab9da79895c23e4fa194b7e0dd9cc6b39ce75fd1
Diaa-E <blarb21@yahoo.com>
2023-04-15 12:22:32 +0200

    Fix utility import, assign class to body

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,14 @@
 "use strict";
 
+import domUtility from "./dom.utility";
+
 export function display()
 {
 
     function initDisplay()
     {
         const body = document.querySelector("body");
+        domUtility.addClasses(body, ["main-grid"]);
     }
 
     return {
