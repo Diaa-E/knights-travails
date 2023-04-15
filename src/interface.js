0975b0b02eee7e032fdaffa0002db9b4d7204637
Diaa-E <blarb21@yahoo.com>
2023-04-15 11:57:59 +0200

    Create an interface module

---
+++ src/interface.js
@@ -0,0 +1,14 @@
+"use strict";
+
+export function display()
+{
+
+    function initDisplay()
+    {
+        const body = document.querySelector("body");
+    }
+
+    return {
+        initDisplay: initDisplay,
+    }
+}
