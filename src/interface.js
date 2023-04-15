c3870c5651210b705142afb574537ea0dc85958b
Diaa-E <blarb21@yahoo.com>
2023-04-15 14:23:27 +0200

    Add controls

--- src/interface.js
+++ src/interface.js
@@ -12,6 +12,7 @@ export function display()
         domUtility.addClasses(body, ["main-grid"]);
 
         body.append(header());
+        body.append(controls())
     }
 
     return {
@@ -26,4 +27,26 @@ function header()
     domUtility.addClasses(imgLogo, ["logo"]);
 
     return imgLogo;
+}
+
+function controls()
+{
+    const divWrapper = document.createElement("div");
+    domUtility.addClasses(divWrapper, ["controls-wrapper"]);
+
+    const btnGoCrazy = document.createElement("button");
+    domUtility.addClasses(btnGoCrazy, ["button-regular", "button-special"]);
+    btnGoCrazy.innerText = "GO CRAZY!";
+
+    const btnClear = document.createElement("button");
+    domUtility.addClasses(btnClear, ["button-regular"]);
+    btnClear.innerText = "Clear Log";
+
+    const btnReset = document.createElement("button");
+    domUtility.addClasses(btnReset, ["button-regular"]);
+    btnReset.innerText = "Reset";
+
+    divWrapper.append(btnGoCrazy, btnClear, btnReset);
+
+    return divWrapper;
 }
